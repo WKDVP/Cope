11f22443a63556000f904fea0e98af2c69ebbc90
WKDVP <119437899+WKDVP@users.noreply.github.com>
2023-10-17 01:05:42 +0900

    진짜 찐 최종

--- test.java
+++ test.java
@@ -10,7 +10,7 @@ class Player {
     }
 
     Scanner scan = new Scanner(System.in);
-    Random rand = new Random();
+    // Random rand = new Random();
     String name;
     int bet;
     int money = 10000;
@@ -19,30 +19,25 @@ class Player {
     boolean checkSum = true;
     boolean burst = false;
     boolean isBlackjack = false;
-    ArrayList<Integer> cardNumber = new ArrayList<>();
-    ArrayList<String> cardMark = new ArrayList<>();
+    ArrayList<Card> cards = new ArrayList<>();
     String markList[] = { "하트 ", "크로바 ", "스페이드 ", "다이아 " };
 
-    void getCardFirst() {
-        this.cardNumber.add(rand.nextInt(10) + 1);
-        this.cardNumber.add(rand.nextInt(10) + 1);
-        /*
-         * this.cardNumber.add(10);
-         * this.cardNumber.add(11);
-         */ // 블랙잭 나오게 하기
-        this.cardMark.add(markList[rand.nextInt(4)]);
-        this.cardMark.add(markList[rand.nextInt(4)]);
-        System.out.print(this.name + "의 카드는 " + this.cardMark.get(0) + this.cardNumber.get(0) + ", "
-                + this.cardMark.get(1) + this.cardNumber.get(1)
+    void getCardFirst(Card a, Card b) {
+        Card aC = new Card(a);
+        Card bC = new Card(b);
+        this.cards.add(aC);
+        this.cards.add(bC);
+        System.out.print(this.name + "의 카드는 " + cards.get(0).mark + this.cards.get(0).number + ", "
+                + this.cards.get(1).mark + this.cards.get(1).number
                 + " 입니다. (합 :");
         this.checkSave();
     }
 
-    void getCard() {
-        this.cardNumber.add(rand.nextInt(10) + 1);
-        this.cardMark.add(markList[rand.nextInt(4)]);
-        System.out.print(this.name + "의 카드는 " + this.cardMark.get(cardMark.size() - 1)
-                + this.cardNumber.get(cardNumber.size() - 1) +
+    void getCard(Card a) {
+        Card aC = new Card(a);
+        this.cards.add(aC);
+        System.out.print(this.name + "의 카드는 " + this.cards.get(cards.size() - 1).mark
+                + this.cards.get(cards.size() - 1).number +
                 "입니다. (합: ");
         this.checkSave();
     }
@@ -57,11 +52,11 @@ class Player {
                 scan.nextLine();
             }
 
-            if (this.bet > this.money) {
+            if (this.bet > this.money)
                 System.out.println("배팅 금액이 소지금 보다 많습니다");
-            } else if (this.bet <= 0) {
+            else if (this.bet <= 0)
                 System.out.println("베팅 금액이 있어야 합니다");
-            } else {
+            else {
                 this.money -= bet;
                 break;
             }
@@ -69,11 +64,16 @@ class Player {
 
     }
 
-    void checkSave() {
+    void getSum() {
         this.sum = 0;
-        for (int i = 0; i < this.cardNumber.size(); i++) {
-            this.sum += this.cardNumber.get(i);
+        for (Card card : this.cards) {
+            this.sum += card.mean;
         }
+    }
+
+    void checkSave() {
+        getSum();
+        aTo11();
         if (this.sum > burstNum) {
             System.out.println(sum + ") 버스트!");
             // this.sum = 0;
@@ -103,11 +103,26 @@ class Player {
     }
 
     void restart() {
-        this.cardMark.clear();
-        this.cardNumber.clear();
+        this.cards.clear();
         this.checkSum = true;
         this.isBlackjack = false;
         this.burst = false;
+        this.bet = 0;
+    }
+
+    void aTo11() {
+        if (this.sum > 21) { // 21넘음?
+            for (int i = 0; i < this.cards.size(); i++) { // ㅇㅇ
+                if (this.cards.get(i).mean == 11 && sum > 21) { // A있음?
+                    this.cards.get(i).mean = 1; // ㅇㅇ
+                    getSum();
+                    aTo11();
+                } else {
+                    burst = true;
+                }
+            }
+            // ㄴㄴ
+        }
     }
 
 }
@@ -131,22 +146,15 @@ class Npc extends Player {
         this.money -= this.bet;
     }
 
-    void getSum() {
-        this.sum = 0;
-        for (int i = 0; i < this.cardNumber.size(); i++) {
-            this.sum += this.cardNumber.get(i);
-        }
-    }
-
-    void stayOrHit() {
+    void stayOrHit(Card a) {
         this.getSum();
         if (this.sum < 13) {
             // this.sum = 0;
-            this.getCard();
+            this.getCard(a);
         } else if (this.sum < 16) {
             // this.sum = 0;
             if (rand.nextInt(100) < 50) {
-                this.getCard();
+                this.getCard(a);
             } else {
                 this.checkSum = false;
                 this.getSum();
@@ -155,7 +163,7 @@ class Npc extends Player {
         } else if (this.sum < 19) {
             // this.sum = 0;
             if (rand.nextInt(100) < 10) {
-                this.getCard();
+                this.getCard(a);
             } else {
                 this.checkSum = false;
                 this.getSum();
@@ -176,44 +184,45 @@ class Dealer extends Player {
         this.name = "딜러";
     }
 
-    void getCardFirst() {
-        this.cardNumber.add(rand.nextInt(10) + 1);
-        this.cardNumber.add(rand.nextInt(10) + 1);
-        //this.cardNumber.add(1);
-        //this.cardNumber.add(20);
+    void getCardFirst(Card a, Card b) {
+        Card aC = new Card(a);
+        Card bC = new Card(b);
+        this.cards.add(aC);
+        this.cards.add(bC);
+        // this.cards.add(1);
+        // this.cards.add(20);
         // 블랙잭 나오게 하기
-        this.cardMark.add(markList[rand.nextInt(4)]);
-        this.cardMark.add(markList[rand.nextInt(4)]);
-        System.out.println(this.name + "의 카드는 " + this.cardMark.get(1) + this.cardNumber.get(1) + " 입니다.");
+        System.out.println(this.name + "의 카드는 " + this.cards.get(1).mark + this.cards.get(1).number + " 입니다.");
     }
 
     void getSum() {
         this.sum = 0;
-        for (int i = 1; i < this.cardNumber.size(); i++) {
-            this.sum += this.cardNumber.get(i);
+        for (int i = 1; i < this.cards.size(); i++) {
+            this.sum += this.cards.get(i).mean;
         }
     }
 
-    void getCard() {
-        this.cardNumber.add(rand.nextInt(10) + 1);
-        this.cardMark.add(markList[rand.nextInt(4)]);
-        System.out.print(this.name + "의 카드는 " + this.cardMark.get(cardMark.size() - 1)
-                + this.cardNumber.get(cardNumber.size() - 1) +
+    void getCard(Card a) {
+        Card aC = new Card(a);
+        this.cards.add(aC);
+        System.out.print(this.name + "의 카드는 " + this.cards.get(cards.size() - 1).mark
+                + this.cards.get(cards.size() - 1).number +
                 "입니다. (합: ");
         this.checkSaveD();
     }
 
     void checkSaveD() {
         this.sum = 0;
-        for (int i = 1; i < this.cardNumber.size(); i++) {
-            this.sum += this.cardNumber.get(i);
+        for (int i = 1; i < this.cards.size(); i++) {
+            this.sum += this.cards.get(i).mean;
         }
         if (this.sum > 21) { // 뒤집은 카드 공개
             System.out.println(sum + ") 딜러 버스트!");
             this.checkSum = false;
             this.burst = true;
         } else if (this.sum > 16) {
-            System.out.print(sum + ")\n딜러의 첫 카드는 " + this.cardMark.get(0) + this.cardNumber.get(0) + " 입니다. (합: ");
+            System.out.print(
+                    sum + ")\n딜러의 첫 카드는 " + this.cards.get(0).mark + this.cards.get(0).number + " 입니다. (합: ");
             this.checkSave();
             // this.sum = 0;
             this.checkSum = false;
@@ -224,14 +233,24 @@ class Dealer extends Player {
     }
 
 }
+
 class Card {
     String mark;
     String number;
     int mean;
-    Card(String a, String b, int c){
+    Card temp;
+
+    Card(String a, String b, int c) {
         mark = a;
         number = b;
         mean = c;
+    }
+
+    Card(Card a) {
+        this.mark = a.mark;
+        this.number = a.number;
+        this.mean = a.mean;
+
     }
 }
 
@@ -244,7 +263,7 @@ public class test {
         try {
             Thread.sleep(a * 1000);
         } catch (InterruptedException e) {
-
+            Thread.currentThread().interrupt();
         }
     }
 
@@ -256,19 +275,17 @@ public class test {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("이름을 입력하세요: ");
-        Player you = new Player(scan.nextLine());
-        Npc npc1 = new Npc();
-        Npc npc2 = new Npc();
-        Dealer dealer = new Dealer();
-        //ArrayList<Card> cards = new ArrayList<>();
+        Random rand = new Random();
         String[] markList = { "하트 ", "크로바 ", "스페이드 ", "다이아 " };
-        String[] numberList = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" }; // 13
+        String[] numberList = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A" }; // 13
         Card[] cards = new Card[52];
-        for (int i = 0,index = 0; i < 4; i++) {
+        for (int i = 0, index = 0; i < 4; i++) {
             for (int j = 0; j < numberList.length; j++) {
-                if (j < 10) {
-                    cards[index] = new Card(markList[i], numberList[j], j + 1);
+                if (j < 9) {
+                    cards[index] = new Card(markList[i], numberList[j], j + 2);
+                    index++;
+                } else if (j == 12) {
+                    cards[index] = new Card(markList[i], numberList[j], 11);
                     index++;
                 } else {
                     cards[index] = new Card(markList[i], numberList[j], 10);
@@ -276,23 +293,31 @@ public class test {
                 }
             }
         }
-        for(int a = 0; a<52;a++){
-            System.out.println(a + cards[a].mark + cards[a].number + cards[a].mean);
-        }
-        
-        while (true) {
+        /*
+         * for (int a = 0; a < 52; a++) {
+         * System.out.println(a + cards[a].mark + cards[a].number + cards[a].mean);
+         * }
+         */
+        System.out.print("이름을 입력하세요: ");
+        Player you = new Player(scan.nextLine());
+        Npc npc1 = new Npc();
+        Npc npc2 = new Npc();
+        Dealer dealer = new Dealer();
+        Boolean keepGoing = true;
+        while (keepGoing) {
             you.betting();
             sleep(1);
             npc1.betting();
             sleep(1);
             npc2.betting();
-            you.getCardFirst();
+            // you.getCardFirst(cards[12], cards[0]);
+            you.getCardFirst(cards[rand.nextInt(cards.length)], cards[rand.nextInt(cards.length)]);
             sleep(1);
-            npc1.getCardFirst();
+            npc1.getCardFirst(cards[rand.nextInt(cards.length)], cards[rand.nextInt(cards.length)]);
             sleep(1);
-            npc2.getCardFirst();
+            npc2.getCardFirst(cards[rand.nextInt(cards.length)], cards[rand.nextInt(cards.length)]);
             sleep(1);
-            dealer.getCardFirst();
+            dealer.getCardFirst(cards[rand.nextInt(cards.length)], cards[rand.nextInt(cards.length)]);
 
             while (you.checkSum || npc1.checkSum || npc2.checkSum) {
                 while (you.checkSum) {
@@ -301,7 +326,7 @@ public class test {
                     if (temp.equals("s")) {
                         you.checkSum = false;
                     } else if (temp.equals("h")) {
-                        you.getCard();
+                        you.getCard(cards[rand.nextInt(cards.length)]);// cards[rand.nextInt(cards.length)
                         break;
                     } else {
                         System.out.println("잘못된 입력입니다");
@@ -311,18 +336,17 @@ public class test {
 
                 if (npc1.checkSum) {
                     sleep(3);
-                    npc1.stayOrHit();
+                    npc1.stayOrHit(cards[rand.nextInt(cards.length)]);
                 }
                 if (npc2.checkSum) {
                     sleep(2);
-                    npc2.stayOrHit();
+                    npc2.stayOrHit(cards[rand.nextInt(cards.length)]);
                 }
             }
             while (dealer.checkSum) {
                 sleep(2);
-                dealer.getCard();
+                dealer.getCard(cards[rand.nextInt(cards.length)]);
             }
-            // 대충 계산하는 코드 넣죠?
 
             if (dealer.burst) { // 딜러가 버스트
                 if (!you.burst) {
@@ -349,27 +373,24 @@ public class test {
                 check(21 - dealer.sum, npc1);
                 check(21 - dealer.sum, npc2);
             }
-            /*
-             * String temp2;
-             * try {
-             * temp2 = scan.nextLine();
-             * } catch (InputMismatchException e) {
-             * System.out.println("옳바른 정수값을 입력해 주세요");
-             * scan.nextLine();
-             * }
-             */
             System.out.print("계속하시겠습니까?(y/n): ");
             String temp2 = scan.nextLine();
-            if (temp2.equals("n")) {
-                break;
-            } else if (you.money <= 0) {
-                System.out.println("돈이 부족합니다.");
-                break;
-            } else {
-                you.restart();;
-                npc1.restart();
-                npc2.restart();
-                dealer.restart();
+            while (true) {
+                if (temp2.equals("n")) {
+                    keepGoing = false;
+                    break;
+                } else if (you.money <= 0) {
+                    System.out.println("돈이 부족합니다.");
+                    keepGoing = false;
+                    break;
+                } else {
+                    you.restart();
+                    npc1.restart();
+                    npc2.restart();
+                    dealer.restart();
+                    break;
+                }
+
             }
 
         }
